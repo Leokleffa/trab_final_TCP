f05475d7bdf12b433e86d5731f8b6d46a80a1130
NegrY <48329860+0NegrY0@users.noreply.github.com>
2023-01-15 17:52:50 -0300

    Update Open.java
    Primeira versao de busca de arquivo de origem, abertura e leitura

--- src/File/Open.java
+++ src/File/Open.java
@@ -1,5 +1,80 @@
 package File;
 
-public class Open {
+import javax.swing.JFileChooser;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
+public class Open {
+  
+  private String pathFile;
+	private String fileContent;
+	private String pathMaster;
+	
+
+
+	public void pathFileSelector(){
+		JFileChooser chooser = new JFileChooser();
+		chooser.showOpenDialog(null);
+		File f = chooser.getSelectedFile();
+		String filename = f.getAbsolutePath();
+		this.pathFile = filename;
+	}
+	
+  
+	
+	public void scanFile() throws FileNotFoundException {
+		File file = new File(this.pathFile);
+		Scanner scan = new Scanner(file);
+		while(scan.hasNextLine()) {
+			this.fileContent = fileContent.concat(scan.nextLine() + "\n");
+		}
+		scan.close();
+	}
+	
+  
+	
+	public void copyToMaster() throws IOException {
+		FileWriter writer = new FileWriter(this.pathMaster); 
+		writer.write(this.fileContent);
+		writer.close();
+	}
+	
+	
+  
+	public String getPathFile() {
+		return pathFile;
+	}
+
+
+
+
+	public void setPathFile(String pathFile) {
+		this.pathFile = pathFile;
+	}
+
+
+
+	public String getFileContent() {
+		return fileContent;
+	}
+
+
+
+	public void setFileContent(String fileContent) {
+		this.fileContent = fileContent;
+	}
+
+
+	public String getPathMaster() {
+		return pathMaster;
+	}
+
+
+	public void setPathMaster(String pathMaster) {
+		this.pathMaster = pathMaster;
+	}
+	
 }
