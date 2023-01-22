2aff4a0e31096983b813dbe0cee54b316276f4df
Leokleffa <102036917+Leokleffa@users.noreply.github.com>
2023-01-22 18:12:28 -0300

    Update MsgHelp.java

--- src/gui/components/menu/MsgHelp.java
+++ src/gui/components/menu/MsgHelp.java
@@ -19,7 +19,7 @@ public class MsgHelp extends JFrame {
 	 */
 	public MsgHelp() {
 		setTitle("Help");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
