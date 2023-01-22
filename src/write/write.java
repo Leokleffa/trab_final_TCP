afa09279161563fa6378845c83de2be2718f5c25
NegrY <48329860+0NegrY0@users.noreply.github.com>
2023-01-22 18:34:54 -0300

    Create write.java
    no momento inutil

---
+++ src/write/write.java
@@ -0,0 +1,40 @@
+package write;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class write {
+
+	private String textContent;
+	private String filePath;
+
+	
+	public void writeText() throws IOException{
+		Path fileName = Path.of(this.filePath);
+		
+		Files.writeString(fileName, this.textContent);
+	}
+
+
+	public String getTextContent() {
+		return textContent;
+	}
+
+
+	public void setTextContent(String textContent) {
+		this.textContent = textContent;
+	}
+
+
+	public String getFilePath() {
+		return filePath;
+	}
+
+
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+	
+	
+}
