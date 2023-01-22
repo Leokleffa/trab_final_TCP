150106b9105c2f6e7f29903dbeaae6b907f299ca
NegrY <48329860+0NegrY0@users.noreply.github.com>
2023-01-22 18:28:51 -0300

    Create open.java

---
+++ src/open/open.java
@@ -0,0 +1,74 @@
+package open;
+
+import javax.swing.JFileChooser;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+
+
+public class open {
+	
+	private String pathFile;
+	private String fileContent;
+	private String pathMaster;
+	
+
+
+	public void pathFileSelector(){
+		JFileChooser chooser = new JFileChooser();
+		chooser.showOpenDialog(null);
+		File f = chooser.getSelectedFile();
+		String filename = f.getAbsolutePath();
+		this.pathFile = filename;
+	}
+	
+	
+	public void scanFile() throws FileNotFoundException {
+		File file = new File(this.pathFile);
+		Scanner scan = new Scanner(file);
+		while(scan.hasNextLine()) {
+			this.fileContent = fileContent.concat(scan.nextLine() + "\n");
+		}
+		scan.close();
+	}
+	
+	
+	
+	public String getPathFile() {
+		return pathFile;
+	}
+
+
+
+
+	public void setPathFile(String pathFile) {
+		this.pathFile = pathFile;
+	}
+
+
+
+	public String getFileContent() {
+		return fileContent;
+	}
+
+
+
+	public void setFileContent(String fileContent) {
+		this.fileContent = fileContent;
+	}
+
+
+	public String getPathMaster() {
+		return pathMaster;
+	}
+
+
+	public void setPathMaster(String pathMaster) {
+		this.pathMaster = pathMaster;
+	}
+	
+}
