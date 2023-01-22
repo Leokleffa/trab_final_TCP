d827f292bddc9954b8fcbaa7c72f1c9f5ffd087e
NegrY <48329860+0NegrY0@users.noreply.github.com>
2023-01-22 19:06:28 -0300

    Update ActionListenerOpen.java

--- src/file/ActionListenerOpen.java
+++ src/file/ActionListenerOpen.java
@@ -6,13 +6,19 @@ import gui.components.Screen;
 
 public class ActionListenerOpen implements ActionListener {
 	//coloca atributos para receber os argumentos
+	private String filePath;
+	private String fileContent;
 	
-	public ActionListenerOpen(/*Argumentos do OPEN*/){
+	public ActionListenerOpen(String filePath, String fileContent){
 		//this.argumentos = argumentos;
+		this.filePath = filePath;
+		this.fileContent = fileContent;		
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		Screen.setTextScreen("OPEN FILE!");
 	}
+	
+	
 }
