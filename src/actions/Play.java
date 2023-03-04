1beba8733ddf1b360aca2de823717919a323ef20
Igormartinssilva <92041109+Igormartinssilva@users.noreply.github.com>
2023-03-04 15:07:26 -0300

    Update Play.java

--- src/actions/Play.java
+++ src/actions/Play.java
@@ -1,141 +1,245 @@
+/*Bom, o que eu pensei para a implementação do play foi receber a string do editor de texto
+e percorrer caracter por caracter para analisar as exeções. A principio penso que funcina essa logica,
+mas tem que pensar nas exeçoes. 
+
+O QUE FALTA FAZER
+-- SELECIONAR ALEATORIAMENTE UM INSTRUMENTO(ACHO QUE FAZ UM VETOR COM VARIOS E CRIA UM METODO DE SELECIONAR ELES)
+-- MMETODO PARA SETAR ALEATORIAMENTE UM BPM
+-- METODO PARA TOCAR: a PRINCIPIO E SO FAZER UM PLAYER.PLAY(Texto), mas tem que tocar so uma nota. talvez tenha execeços, por que tem 
+coisas que a Jfugue so entende se for um conjunto de caracteres.
+-- CONSEGUIR PEGAR O TEXTO DO PAINEL: acho que é erro de sintaxe, mas nao consegui fazer pegar, nao entendi o meu erro.
+-- ARRUMAR ALGUNS ERROS: e mais o chamamento de funções que por algum motivo da erro, acho que é so sintaxe o erro.
+-- PENSAR NAS EXCEÇOES: deixei apontado algumas exceçoes que talvez tenha
+
+vou deixar comentado tudo, como funciona na jfugue e como pensei pra implementar isso no nosso caso.
+
+Qualquer dúvida, não exite em me chamar.
+att, Igor M. Silva ;)
+
+*/
+
+
 package actions;
 
 
 import org.jfugue.player.*;
 import org.jfugue.pattern.Pattern;
+import gui.components.EditorText;
 
 public class Play {
 
     Player tocaMusica = new Player();
     Pattern pattern = new Pattern();
-
-    
-    /*tem que por o texto todo do painel numa string
-    e fazer a função para retornar essa string para poder 
-    ir andando nela caracter por caracter
-    */
+    String Telphone ="I[TELEPHONE_RING] ";
+    int novoVol;
+    String VolDefault;
+    int caracterAnt;
+    String moreBPM;
     String textPainel;
-    
-   textPainel = getTextPainelEditor();
-   int i = 0;
+    int oitava=5; //por padrao, a Jfugue usa a oitava no 5. Ela vai de 0 a 10
+    int i = 0;//variavel para saber a posição da string
+     
+    //aqui pega o texto do painel e passa para uma string
+    ////NAO SEI PORQUE DA ERRO, ACHO QUE É SINTAXO
+   textPainel = getTextEditorPane();
+  
    
-   //tem arrumar os nomes dessas variaveis
-   public int setVolumeDefautl()
+   //metedo para deixar o volume setado no default
+   public int setVolumeDefautl(int novoVol)
    {
       //como aumenta o volume
-                /*Pattern pattern1 = new Pattern(":CE(935, 2000) C C C C C C C C C C C C C C C");
+                /*Pattern pattern1 = new Pattern(":CE(935, 2000) C A G C C C C C C C C C C C C");
                 new Player().play(pattern1);
-                onde CE é ontroller Event, 935 o codigo para mecher no volume e 2000(0 a 16383) é 
+                onde CE é ontroller Event, 935 o codigo para mecher no volume e 2000(de 0 a 16383) é 
                 a altura do volume*/
        int volDefault = 2000;
-       //como nao consegui passar int ou string no lugar do 2000
-       //pode-se concatenar :CE(935,  com NovoVol) mais o resante do texto
-       //talvez tem que fazer tudo strng e usar int to string
-       int novoVol = volDefault;
+       novoVol = volDefault;
+       
       return novoVol;
    
    }
-   public int setDoubleVol(int novoVol)
+   
+   //metodo para dobrar o volume
+   public String setDoubleVol(int novoVol, String textPainel)
    {
-       return novoVol = novoVol * 2;
+       String cabecalhoVol=":CE(935,";//como precissa concatenar isso com a string TextPainel, temos que ter esse cabeçalho padrao pronto
+       String fechaParentes =") ";//tem que tem espaço depois do parenteses pq a Jfugue nao reconhece o aumento do volume
+       String volumeNovo;
        
+       novoVol = novoVol * 2; //dobra o valor do volume. TRATAR EXCEÇÃO, CASO PASSE DO VALOR 16383, DEVE VOLTAR PRA O PADRAO
+       
+       volumeNovo = Integer.toString(novoVol); //converte inteiro para string
+       
+       String volume = cabecalhoVol.concat(volumeNovo.concat(fechaParentes));//saida: "CE(935, volumeNovo) "
+        
+       return textPainel = volume.concat(textPainel);// saida esperada: "CE(935, volumeNovo) resto da string do textpainel"
+       //talvez tenha que pensar em exceçoes, tipo como, se vai funcionar caso faça essa modificação no meio da string
+       //acho que funciona, mas não tenho certeza
    }
    
    //setando o volume default
-    setVolumeDefault();
+    novoVol = setVolumeDefautl(novoVol);
     
-    
-    private boolen getAcordeIgual(String textPainel(caracterAnt))
+    //metodo para saber se o caracter anterior era igual a um dos acordes
+    public Boolean getAcordeIgual(String textPainel, int caracterAnt)
     {
-        int k;
+        int k;//não usar i, pq ja vai estar sendo usado para percorrer o texto
+        char temp;
         String acordes ="ABCDEFG";
+        temp = textPainel.charAt(caracterAnt);
         
-        for(k=0, k<7, k++ )
+        //o for percorre a string dos acordes e compara com o caracter anterior do textPainel
+        for(k=0; k<7; k++ )
         {
-            if( textPainel(caracterAnt) == acordes[k])
+            if( temp == acordes.charAt(k))
                 return true;           
         }
         return false;
     }
     
-    private String defaultBPM()
+    //metodo para setar um BPM default
+    private String defaultBPM(String moreBPM)
     {
+        //exemplo de toque de BPM
+                   // player.play("T320 A A A A T160  D E F G A B T240  D E F G A B");
         String normalBPM = "70";
-        String moreBPM = normalBPM;
+        moreBPM = normalBPM; /*passo para essa string pq é ela que vamos manipular depois
+                             - sim, poderia ter nome melhor, mas nao sei qual
+                             */
+        
         return moreBPM;
     }
     
-    defaultBPM();
+    moreBPM = defaultBPM(moreBPM);
     
     public String setMoreBPM80(String moreBPM)
     { 
-        int newBPM = atoi(moreBPM);//trasnforma a string para inteiro 
+        int newBPM = Integer.parseInt(moreBPM);//trasnforma a string para inteiro 
+       
         newBPM = newBPM + 80;
-        moreBPM = intergerToString(newBPM);
-         
-         return moreBPM;
+        moreBPM = Integer.toString(newBPM);//transforma inteiro para string
+        moreBPM = "T".concat(moreBPM);//saida esperada: "TmoreBPM(em numero)"
         
+        return moreBPM;
+    }
+    
+    
+    public String setNewInstrument(String textPainel)
+    {
+        String newInstrument = "I[SOUNDTRACK] ";// pensei nesse instrumento para tocar, fica parecendo musica de video game
+        
+        textPainel = newInstrument.concat(textPainel);
+        
+        return textPainel;
+    }
+    
+    //metodo para aumnetar uma oitava
+    public String setOitavaPlus(int oitava)
+    {
+        String oitavaString;
+        //como vai de 0 a 10 as oitavas na Jfugue, quando chegar no 10, tem que recomeçar do 0
+        if(oitava<10)
+            oitava = oitava + 1;
+        else if(oitava==10)
+            oitava=0;
+            
+        oitavaString = Integer.toString(oitava);
+       
+        return oitavaString;
     }
    
-    while(textPainel(i) =! null  || getStatusBottonPause() == false )
+    public String setOitavaLess(int oitava)
     {
-        switch (textPainel(i)) //i é a posição do caracter do texto do painel
+        String oitavaString;
+        
+        if(oitava>0)
+            oitava = oitava - 1;
+        else if(oitava==0)
+            oitava = 10;
+        
+        oitavaString = Integer.toString(oitava);
+       
+        return oitavaString;
+    }
+    
+    //aqui fica num loop ate chegar no fim da string ou ativarem o botao pause(NAO SEI SE VAMOS IMPLEMENTAR O PAUSE)
+    while(textPainel.charAt(i) =! null  || getStatusBottonPause() == false )
+    {
+        //caso o caracter atual for tal... faça...
+        switch (textPainel.charAt(i)) //i é a posição do caracter do texto do painel
         {
-            case '+':           //aumentar o volume
-                //como aumenta o volume
-                /*Pattern pattern1 = new Pattern(":CE(935, 2000) C C C C C C C C C C C C C C C");
-                new Player().play(pattern1);
-                onde CE é ontroller Event, 935 o codigo para mecher no volume e 2000(0 a 16383) é 
-                a altura do volume*/
+            case '+':           //aumentar o volume            
+                setDoubleVol(novoVol, textPainel);
+
+                break;
                 
-                setDoubleVol(vol[Volume]);
-
-                break;
             case '-':           //volta pro volume dafault
-                setVolumeDefault();
+                 
+                novoVol = setVolumeDefautl(novoVol);
+                VolDefault = Integer.toString(novoVol);
+                VolDefault = "CE(935,".concat(VolDefault);
+                VolDefault = VolDefault.concat(") ");
+                textPainel =  VolDefault.concat(textPainel);
 
                 break;
+                
             case 'O'://se for nota anterior for de A a G, repete a nota 
-                //para esse if, poderia colocar tudo em uma classe com for para passar pelas notas. Dai ficaria if(textPainel =! getacordes())...
-              int caracterAnt = i-1;
-                //if (textPainel(i - 1) == 'A' || textPainel(i - 1) == 'B' || textPainel(i - 1) == 'C' || textPainel(i - 1) == 'D' || textPainel(i - 1) == 'E' || textPainel(i - 1) == 'F' || textPainel(i - 1) == 'G') 
-                if(!getAcordeIgual(textPainel(caracterAnt)))  
+              
+                caracterAnt = i-1;
+                if(getAcordeIgual(textPainel, caracterAnt))
                 {
-                        tocaMusica(textPainel());
+                        tocaMusica(textPainel.charAt(i));
                     
                 }
-                else //se nao, toca um telefone(124)
+                else //se nao, toca um telefone
                     {
                         //basicamente concatenar I[TELEPHONE_RING] com a string do texto. Exemplo:
-                        player.play("I[TELEPHONE_RING] A A A A a a a a D E F G A B");
-                        playTelephone();//criar um metodo para concatenar;
+                        //player.play("I[TELEPHONE_RING] A A A A a a a a D E F G A B");
+                        
+                        textPainel = Telphone.concat(textPainel);//aqui concatena o telefone no inicio da string
+                        //outro metodo de concatenar
+                       // textPainel = Telphone + textPainel;
                     }
 
                 break;
-            case 'I':
-                //if (textPainel(i - 1) == 'A' || textPainel(i - 1) == 'B' || textPainel(i - 1) == 'C' || textPainel(i - 1) == 'D' || textPainel(i - 1) == 'E' || textPainel(i - 1) == 'F' || textPainel(i - 1) == 'G') 
-                 if(!getAcordeIgual(textPainel(caracterAnt))) 
+                
+            case 'I'://se for nota anterior for de A a G, repete a nota 
+                
+                caracterAnt = i-1; 
+                if(getAcordeIgual(textPainel, caracterAnt)) 
                 {
-                    tocaMusica(textPainel());
-                    
+                    tocaMusica(textPainel.charAt(i));                    
                 }
-                else //se nao, toca um telefone(124)
+                else //se nao, toca um telefone
                 {
-                       //basicamente concatenar I[TELEPHONE_RING] com a string do texto. Exemplo:
-                        player.play("I[TELEPHONE_RING] A A A A a a a a D E F G A B");
-                        playTelephone();//criar um metodo para concatenar;
+                    textPainel= Telphone.concat(textPainel);
                 }
 
                 break;
-            case 'R':
-                if (textPainel(i + 1) == '+') //se for sinal de +, aumenta uma oitava
+                
+            case 'U'://se for nota anterior for de A a G, repete a nota 
+                 
+                caracterAnt = i-1;
+                if(getAcordeIgual(textPainel, caracterAnt)) 
                 {
-                    setOitavaPlus();
+                    tocaMusica(textPainel.charAt(i));                    
+                }
+                else //se nao, toca um telefone
+                {
+                    textPainel= Telphone.concat(textPainel);
+                }
+
+                break;
+                
+            case 'R':
+                if (textPainel.charAt(i+1) == '+') //se for sinal de +, aumenta uma oitava
+                {
+                    textPainel = textPainel.concat(setOitavaPlus(oitava)) ;
 
                 } 
-                else if (textPainel(i + 1) == '-') //se for sinal de -, diminui uma oitava
+                else if (textPainel.charAt(i+1) == '-') //se for sinal de -, diminui uma oitava
                 {
-                    setOitavaLess();
+                    textPainel = textPainel.concat(setOitavaLess(oitava)) ;
                 }
                 break;
 
@@ -144,22 +248,23 @@ public class Play {
                 break;
             
             case '\n':                  //muda para outro instrumento
-                setNewInstrument();
+               textPainel = setNewInstrument(textPainel);
 
                 break;
-            case 'B':
-                if (textPainel(i + 1) == 'P' && textPainel(i + 2) == 'M' && textPainel(i + 3) == '+')
-                { ///aumenta o BPM em 80 unidades
-                    //concatenar BPM(T80) com o texto
-                    player.play("T320 A A A A T160  D E F G A B T240  D E F G A B");
+                
+            case 'B'://aumenta o BPM em 80 unidades se tiver escrito "BPM+"
+                if (textPainel.charAt(i + 1) == 'P' && textPainel.charAt(i + 2) == 'M' && textPainel.charAt(i + 3) == '+')
+                { 
+                   //exemplo de toque de BPM
+                   // player.play("T320 A A A A T160  D E F G A B T240  D E F G A B");
                     moreBPM = setMoreBPM80(moreBPM);
-                       
-                    //aqui concatena mmoreBPM com o textPainel, tem que pesquisar sobre isso
-                    
-                    
+                    moreBPM = moreBPM.concat(" ");//espaço pq e se não o leitor de musica nao le direito 
+                      
+                    textPainel = moreBPM.concat(textPainel);                  
                 }
 
                 break;
+                
             case ';':
                  ///coloca valor aleatorio no BPM
                 setRandomBPM();
@@ -168,7 +273,9 @@ public class Play {
             default://caso nao seja nenhum caso especifico, toca normalmente
                 tocaMusica.play(TextPainel);
                 break;
-
+                //depois de tratar os caracteres especial, ele tem que tocar a musica(tocar uma nota somente)
+                //aqui que nao sei se tem exceção, pois concatenou varias coisas, pode dar algum erro
+                tocaMusica.play(TextPainel);
                 i++;//soma para ir para o proximo caracter da string do painel
         }
     }
